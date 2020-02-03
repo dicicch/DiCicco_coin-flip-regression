089a6ba5e01893121c7bbac8ee10036fa3a5323a
dicicch <hsdicicco@gmail.com>
2020-02-03 18:48:13 -0500

    Requirements

--- project/required/requirements.R
+++ project/required/requirements.R
@@ -1,3 +1,7 @@
-install.packages("data.table",repos= "http://lib.stat.cmu.edu/R/CRAN/")
-install.packages("optparse",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+packages = c("data.table", "optparse",
+             "tidyr", "plyr", "outliers",
+             "glmnet", "corrplot", "e1071")
 
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())))  
+}
